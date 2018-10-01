54d46b384db205cecbd80bea288f0359f8a5a8e0
Rocio Latorre <41635062+rociolatorreortiz@users.noreply.github.com>
2018-10-01 09:58:39 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$nombre = $_POST['nombre'];
+$fechaNaci = $_POST['fechaNaci'];
+$actividad1 = $_POST['actividad1'];
+$actividad = $_POST['actividad'];
+
+
+echo $nombre."<br>";
+echo $fechaNaci."<br>";
+echo $futbol."<br>";
+echo $Baloncesto."<br>";
+echo $Patinaje."<br>";
+echo $Volleyball."<br>";
+echo $Ninguno."<br>";
+echo $actividad."<br>";
